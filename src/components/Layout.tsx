52e41f131cc1f3b5ac969464dd80d2af3f4491bf
[dyad] <asharyalam@iqis.sch.id>
2025-10-15 10:58:03 +0800

    [dyad] Menambahkan sidebar navigasi yang responsif dan memperbarui header. - wrote 3 file(s)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,36 @@
 "use client";
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
+import Sidebar from './Sidebar';
 
 const Layout: React.FC = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-grow container mx-auto py-8 px-4">
-        <Outlet />
-      </main>
+    <div className="flex min-h-screen flex-col">
+      <Header onMenuClick={toggleSidebar} />
+      <div className="flex flex-1">
+        <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar} />
+        {isSidebarOpen && (
+          <div
+            className="fixed inset-0 z-40 bg-black/50 lg:hidden"
+            onClick={closeSidebar}
+          />
+        )}
+        <main className="flex-1 p-4 lg:p-8">
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 };
